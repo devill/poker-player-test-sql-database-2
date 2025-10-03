dfb2eb248457b4877e5476c8fb9c046e463251dd
Ivett Ördög <devillmeister@gmail.com>
2025-10-03 21:09:34 +0200

    Update player.py

--- player.py
+++ player.py
@@ -2,7 +2,7 @@
 class Player:
     VERSION = "Default Python folding player"
 
-    def betRequest(self, game_state):
+    def betRequest(self, game_state)
         return 0
 
     def showdown(self, game_state):
